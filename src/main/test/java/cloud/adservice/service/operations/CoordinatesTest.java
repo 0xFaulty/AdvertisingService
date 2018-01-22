abd236671fdd5d1448c97db4bf52ee26460f746a
0xFaulty <valenteen3d@ya.ru>
2018-01-22 16:09:48 +0300

    test class added

--- src/main/test/java/cloud/adservice/service/operations/CoordinatesTest.java
+++ src/main/test/java/cloud/adservice/service/operations/CoordinatesTest.java
@@ -1,5 +1,13 @@
+package cloud.adservice.service.operations;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CoordinatesTest {
 
+    @Test
+    public void cluster() {
+
+    }
 }
